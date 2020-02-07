1fce4eed2dd5d05089e13b3459be4b17901a7fad
Geoff Clements <ro2kz0@gmail.com>
2020-02-07 19:42:43 +0000

    Coding through the codec

--- slimproto/src/codec.rs
+++ slimproto/src/codec.rs
@@ -1,18 +1,23 @@
-use bytes::{Buf, BufMut, BytesMut};
-use mac_address;
-use tokio::net::UdpSocket;
-use tokio_util::codec;
+use byteorder::{ReadBytesExt, BE};
+use bytes::{
+    buf::{BufExt, BufMut},
+    BytesMut,
+};
+use mac_address::MacAddress;
+use tokio_util::codec::{Decoder, Encoder};
 
-use std::io;
-use std::net::Ipv4Addr;
+use std::{
+    io,
+    net::Ipv4Addr,
+};
 
-struct StatData;
+pub struct StatData;
 
 pub enum ClientMessage {
     Helo {
         device_id: u8,
         revision: u8,
-        mac: mac_address::MacAddress,
+        mac: MacAddress,
         uuid: [u8; 16],
         wlan_channel_list: u16,
         bytes_received: u64,
@@ -56,7 +61,7 @@ pub enum ServerMessage {
 
 pub struct SlimCodec;
 
-impl codec::Encoder for SlimCodec {
+impl Encoder for SlimCodec {
     type Item = ClientMessage;
     type Error = io::Error;
 
@@ -66,7 +71,7 @@ impl codec::Encoder for SlimCodec {
     }
 }
 
-impl codec::Decoder for SlimCodec {
+impl Decoder for SlimCodec {
     type Item = ServerMessage;
     type Error = io::Error;
 
@@ -75,16 +80,20 @@ impl codec::Decoder for SlimCodec {
             return Ok(None);
         };
 
-        let size = (buf[..2].get_u16_be()) as usize;
-        if buf.len() < size + 2 {
-            if buf.capacity() < size + 2 {
-                buf.reserve(size);
+        let frame_size = {
+            let mut r = buf.take(2).reader();
+            r.read_u16::<BE>()? as usize
+        };
+
+        if buf.len() < frame_size + 2 {
+            if buf.capacity() < frame_size + 2 {
+                buf.reserve(frame_size);
             }
             return Ok(None);
         };
 
         buf.split_to(2);
-        let msg = buf.split_to(size);
+        let msg = buf.split_to(frame_size);
 
         match msg.into() {
             ServerMessage::Error => Err(io::Error::new(
@@ -115,155 +124,155 @@ impl From<ClientMessage> for BytesMut {
                 buf.put_u8(revision);
                 buf.put(mac.bytes().as_ref());
                 buf.put(uuid.as_ref());
-                buf.put_u16_be(wlan_channel_list);
-                buf.put_u64_be(bytes_received);
+                buf.put_u16(wlan_channel_list);
+                buf.put_u64(bytes_received);
                 buf.put(capabilities.as_bytes());
             }
 
             ClientMessage::Bye(val) => {
                 buf.put("BYE!".as_bytes());
                 buf.put_u8(val);
-            }
-
+            } 
+            
             ClientMessage::Stat {
-                event_code,
-                stat_data,
-            } => {
-                buf.put("STAT".as_bytes());
-                buf.put(event_code.as_bytes());
-                buf.put_u8(stat_data.crlf);
-                buf.put_u16_be(0);
-                buf.put_u32_be(stat_data.buffer_size);
-                buf.put_u32_be(stat_data.fullness);
-                buf.put_u64_be(stat_data.bytes_received);
-                buf.put_u16_be(stat_data.sig_strength);
-                buf.put_u32_be(stat_data.jiffies);
-                buf.put_u32_be(stat_data.output_buffer_size);
-                buf.put_u32_be(stat_data.output_buffer_fullness);
-                buf.put_u32_be(stat_data.elapsed_seconds);
-                buf.put_u16_be(stat_data.voltage);
-                buf.put_u32_be(stat_data.elapsed_milliseconds);
-                buf.put_u32_be(stat_data.timestamp);
-                buf.put_u16_be(stat_data.error_code);
-            }
+                  event_code,
+                  stat_data,
+              } => {
+              buf.put("STAT".as_bytes());
+              buf.put(event_code.as_bytes());
+            //   buf.put_u8(stat_data.crlf);
+              buf.put_u16(0);
+            //   buf.put_u32(stat_data.buffer_size);
+            //   buf.put_u32(stat_data.fullness);
+            //   buf.put_u64(stat_data.bytes_received);
+            //   buf.put_u16(stat_data.sig_strength);
+            //   buf.put_u32(stat_data.jiffies);
+            //   buf.put_u32(stat_data.output_buffer_size);
+            //   buf.put_u32(stat_data.output_buffer_fullness);
+            //   buf.put_u32(stat_data.elapsed_seconds);
+            //   buf.put_u16(stat_data.voltage);
+            //   buf.put_u32(stat_data.elapsed_milliseconds);
+            //   buf.put_u32(stat_data.timestamp);
+            //   buf.put_u16(stat_data.error_code);
+              }
 
-            ClientMessage::Name(name) => {
-                info!("Sending SETD witn name: {}", name);
-                buf.put("SETD".as_bytes());
-                buf.put_u8(0);
-                buf.put(name.as_bytes());
-            }
+              ClientMessage::Name(name) => {
+            //   info!("Sending SETD witn name: {}", name);
+              buf.put("SETD".as_bytes());
+              buf.put_u8(0);
+              buf.put(name.as_bytes());
+              }
         }
 
         let mut msg_length = Vec::new();
         msg_length.put_u32_le(buf[4..].len() as u32);
         msg_length.into_iter().for_each(|v| buf.insert(4, v));
-        buf.into()
+
+        buf.iter().as_slice().into()
     }
 }
 
 impl From<BytesMut> for ServerMessage {
     fn from(mut src: BytesMut) -> ServerMessage {
-        const GAIN_FACTOR: f64 = 65536.0;
+        //         const GAIN_FACTOR: f64 = 65536.0;
         let msg: String = src.split_to(4).into_iter().map(|c| c as char).collect();
 
         match msg.as_str() {
-            "serv" => {
-                if src.len() < 4 {
-                    ServerMessage::Error
-                } else {
-                    let ip_addr = Ipv4Addr::from(src.split_to(4).into_buf().get_u32_be());
-                    let sync_group = if src.len() > 0 {
-                        Some(
-                            src.take()
-                                .into_iter()
-                                .map(|c| c as char)
-                                .collect::<String>(),
-                        )
-                    } else {
-                        None
-                    };
-                    ServerMessage::Serv {
-                        ip_address: ip_addr,
-                        sync_group_id: sync_group,
-                    }
-                }
-            }
+            //             "serv" => {
+            //                 if src.len() < 4 {
+            //                     ServerMessage::Error
+            //                 } else {
+            //                     let ip_addr = Ipv4Addr::from(src.split_to(4).into_buf().get_u32_be());
+            //                     let sync_group = if src.len() > 0 {
+            //                         Some(
+            //                             src.take()
+            //                                 .into_iter()
+            //                                 .map(|c| c as char)
+            //                                 .collect::<String>(),
+            //                         )
+            //                     } else {
+            //                         None
+            //                     };
+            //                     ServerMessage::Serv {
+            //                         ip_address: ip_addr,
+            //                         sync_group_id: sync_group,
+            //                     }
+            //                 }
+            //             }
 
-            "strm" => {
-                if src.len() < 24 {
-                    return ServerMessage::Error;
-                }
+            //             "strm" => {
+            //                 if src.len() < 24 {
+            //                     return ServerMessage::Error;
+            //                 }
 
-                match src[0] as char {
-                    't' => {
-                        let timestamp = src[14..18].into_buf().get_u32_be();
-                        ServerMessage::Status(timestamp)
-                    }
+            //                 match src[0] as char {
+            //                     't' => {
+            //                         let timestamp = src[14..18].into_buf().get_u32_be();
+            //                         ServerMessage::Status(timestamp)
+            //                     }
 
-                    's' => {
-                        let replay_gain = src[14..18].into_buf().get_u32_be() as f64 / GAIN_FACTOR;
-                        let http_headers = if src.len() >= 24 {
-                            src[24..].into_iter().map(|c| *c as char).collect()
-                        } else {
-                            String::new()
-                        };
-                        ServerMessage::Stream {
-                            autostart: src[1] == b'1' || src[1] == b'3',
-                            threshold: src[7] as u32, // kbytes
-                            output_threshold: src[12] as u64 * 100_000_000, // nanoseconds
-                            replay_gain: replay_gain,
-                            server_port: src[18..20].into_buf().get_u16_be(),
-                            server_ip: Ipv4Addr::from(src[20..24].into_buf().get_u32_be()),
-                            http_headers: http_headers,
-                        }
-                    }
+            //                     's' => {
+            //                         let replay_gain = src[14..18].into_buf().get_u32_be() as f64 / GAIN_FACTOR;
+            //                         let http_headers = if src.len() >= 24 {
+            //                             src[24..].into_iter().map(|c| *c as char).collect()
+            //                         } else {
+            //                             String::new()
+            //                         };
+            //                         ServerMessage::Stream {
+            //                             autostart: src[1] == b'1' || src[1] == b'3',
+            //                             threshold: src[7] as u32, // kbytes
+            //                             output_threshold: src[12] as u64 * 100_000_000, // nanoseconds
+            //                             replay_gain: replay_gain,
+            //                             server_port: src[18..20].into_buf().get_u16_be(),
+            //                             server_ip: Ipv4Addr::from(src[20..24].into_buf().get_u32_be()),
+            //                             http_headers: http_headers,
+            //                         }
+            //                     }
 
-                    'q' => ServerMessage::Stop,
+            //                     'q' => ServerMessage::Stop,
 
-                    'p' => {
-                        let timestamp = src[14..18].into_buf().get_u32_be();
-                        ServerMessage::Pause(timestamp)
-                    }
+            //                     'p' => {
+            //                         let timestamp = src[14..18].into_buf().get_u32_be();
+            //                         ServerMessage::Pause(timestamp)
+            //                     }
 
-                    'u' => {
-                        let timestamp = src[14..18].into_buf().get_u32_be();
-                        ServerMessage::Unpause(timestamp)
-                    }
+            //                     'u' => {
+            //                         let timestamp = src[14..18].into_buf().get_u32_be();
+            //                         ServerMessage::Unpause(timestamp)
+            //                     }
 
-                    'a' => {
-                        let timestamp = src[14..18].into_buf().get_u32_be();
-                        ServerMessage::Skip(timestamp)
-                    }
+            //                     'a' => {
+            //                         let timestamp = src[14..18].into_buf().get_u32_be();
+            //                         ServerMessage::Skip(timestamp)
+            //                     }
 
-                    cmd @ _ => {
-                        let mut msg = msg.to_owned();
-                        msg.push('_');
-                        msg.push(cmd);
-                        ServerMessage::Unrecognised(msg)
-                    }
-                }
-            }
-            "aude" => ServerMessage::Enable(!(src[1].into_buf().get_u8() == 0)),
+            //                     cmd @ _ => {
+            //                         let mut msg = msg.to_owned();
+            //                         msg.push('_');
+            //                         msg.push(cmd);
+            //                         ServerMessage::Unrecognised(msg)
+            //                     }
+            //                 }
+            //             }
+            //             "aude" => ServerMessage::Enable(!(src[1].into_buf().get_u8() == 0)),
 
-            "audg" => ServerMessage::Gain(
-                src[10..14].into_buf().get_u32_be() as f64 / GAIN_FACTOR,
-                src[14..18].into_buf().get_u32_be() as f64 / GAIN_FACTOR,
-            ),
+            //             "audg" => ServerMessage::Gain(
+            //                 src[10..14].into_buf().get_u32_be() as f64 / GAIN_FACTOR,
+            //                 src[14..18].into_buf().get_u32_be() as f64 / GAIN_FACTOR,
+            //             ),
 
-            "setd" => {
-                if src.len() > 1 {
-                    let name: String = src[1..].into_iter().map(|c| *c as char).collect();
-                    ServerMessage::Setname(name)
-                } else {
-                    if src[0] == 0 {
-                        ServerMessage::Queryname
-                    } else {
-                        ServerMessage::Unknownsetd(src[0])
-                    }
-                }
-            }
-
+            //             "setd" => {
+            //                 if src.len() > 1 {
+            //                     let name: String = src[1..].into_iter().map(|c| *c as char).collect();
+            //                     ServerMessage::Setname(name)
+            //                 } else {
+            //                     if src[0] == 0 {
+            //                         ServerMessage::Queryname
+            //                     } else {
+            //                         ServerMessage::Unknownsetd(src[0])
+            //                     }
+            //                 }
+            //             }
             cmd @ _ => ServerMessage::Unrecognised(cmd.to_owned()),
         }
     }
--- slimproto/src/proto.rs
+++ slimproto/src/proto.rs
@@ -1,18 +1,20 @@
 use crate::codec::SlimCodec;
 use crate::discovery;
 
-use tokio::net::TcpStream;
+use tokio::{io::BufStream, net::TcpStream};
 use tokio_util::codec::Framed;
 
 use std::{io, net::Ipv4Addr};
 
 pub struct SlimProto {
-    framed: Framed<TcpStream, SlimCodec>,
-};
+    framed: Framed<BufStream<TcpStream>, SlimCodec>,
+}
 
 impl SlimProto {
     pub async fn new(server_addr: Option<Ipv4Addr>) -> io::Result<Self> {
         const SLIM_PORT: u16 = 3483;
+        const READBUFSIZE: usize = 1024;
+        const WRITEBUFSIZE: usize = 1024;
 
         let server_addr =
             if let Some(addr) = server_addr.or(discovery::discover(None).await?.map(|(a, _)| a)) {
@@ -22,7 +24,7 @@ impl SlimProto {
             };
 
         let server_tcp = TcpStream::connect((server_addr, SLIM_PORT)).await?;
-        let framed = Framed::new(server_tcp, SlimCodec);
+        let framed = Framed::new(BufStream::with_capacity(READBUFSIZE, WRITEBUFSIZE, server_tcp), SlimCodec);
 
         Ok(SlimProto {framed})
     }
